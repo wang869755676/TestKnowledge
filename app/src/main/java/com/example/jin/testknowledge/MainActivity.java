9e79ab74ddf8916010a6d61d84c2cc709b2359b9
869755676@qq.com <869755676@qq.com>
2017-06-06 11:18:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jin/testknowledge/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.jin.testknowledge;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.util.LruCache;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e(TAG, "onCreate: " );
+       /* Fragment fragment=new FragementA();
+        getSupportFragmentManager().beginTransaction().add(R.id.activity_main,fragment).commit();*/
+      //  LruCache
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart: " );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume: " );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause: " );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop: " );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy: " );
+    }
+}
